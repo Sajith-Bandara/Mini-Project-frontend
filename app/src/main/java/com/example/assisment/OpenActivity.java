b36cb902da9c26f664b78b99e2cea125294d9793
Sajith-Bandara <sajithbandara091@gmail.com>
2024-11-07 12:58:00 +0530

    subscriptions page

--- app/src/main/java/com/example/assisment/OpenActivity.java
+++ app/src/main/java/com/example/assisment/OpenActivity.java
@@ -1,6 +1,9 @@
 package com.example.assisment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class OpenActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_open);
+
+        button = findViewById(R.id.submitOpen);
+        Intent intent = new Intent(this,SubActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/assisment/SubscribedActivity.java
+++ app/src/main/java/com/example/assisment/SubscribedActivity.java
@@ -1,6 +1,10 @@
 package com.example.assisment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SubscribedActivity extends AppCompatActivity {
 
+    private Button button;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_subscribed);
+
+        button = findViewById(R.id.deleteSub);
+        Intent intent = new Intent(this,HomeActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
